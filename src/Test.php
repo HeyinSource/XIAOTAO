33a192713ede5eeaf929a924a521616b7fc5be6c
lt <751971678@qq.com>
2021-10-21 14:58:04 +0800

    fix

---
+++ src/Test.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lt
+ * Date: 2021/10/21
+ * Time: 14:51
+ */
+namespace XIAOTAO;
+class Test
+{
+    public static function Test()
+    {
+        echo 'hello word';
+    }
+}
